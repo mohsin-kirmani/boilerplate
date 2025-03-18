f2ffe86d41bd8811e0d26ab34bb466dd6dd994af
Mohsin Kirmani <mohsin007kirmani@gmail.com>
2025-03-18 03:24:21 +0000

    Setup the boilerplate template.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="your_project_name",
+    version="0.1.0",
+    description="A brief description of your project",
+    author="Mohsin Kirmani",
+    packages=["your_project_name"],
+    install_requires=[
+        "requests==2.28.2",
+        "numpy==1.24.2",
+        "pandas==1.5.3",
+        "matplotlib==3.7.1",
+        "scikit-learn==1.2.0",
+        "flask==2.2.3",
+        "pytest==7.2.2",
+    ],
+)
