c24de63f60d313d19ae2bd6f4ecc205e63b98367
Mohsin Kirmani <mohsin007kirmani@gmail.com>
2025-03-20 12:34:55 +0000

    Add the folder structure for tests in the boilerplate template.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="your_project_name",
     version="0.1.0",
     description="A brief description of your project",
     author="Mohsin Kirmani",
-    packages=["your_project_name"],
+    packages=find_packages(),
     install_requires=[
         "requests==2.28.2",
         "numpy==1.24.2",
@@ -13,6 +13,5 @@ setup(
         "matplotlib==3.7.1",
         "scikit-learn==1.2.0",
         "flask==2.2.3",
-        "pytest==7.2.2",
     ],
 )
